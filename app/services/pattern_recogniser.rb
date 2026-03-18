114ab4ffea43c0d4dfde882600d279e343de3432
David Sauntson <davidsauntson@gmail.com>
2026-03-18 14:30:16 +0000

    feat: empty pattern recog and cycle classes

--- app/services/pattern_recogniser.rb
+++ app/services/pattern_recogniser.rb
@@ -1,15 +1,13 @@
 class PatternRecogniser
-
     def initialize(numbers)
         @numbers = numbers
     end
 
     def find_patterns
-        return 
+        return
             [
-                Cycle.new(pattern: [1, 2], repeats: 1),
-                Cycle.new(pattern: [1, 2, 3, 4], repeats: 1)
+                Cycle.new(pattern: [ 1, 2 ], repeats: 1),
+                Cycle.new(pattern: [ 1, 2, 3, 4 ], repeats: 1)
             ]
     end
-
 end
